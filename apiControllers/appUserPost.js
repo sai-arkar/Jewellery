1d01f933f46f78737851b2a27e657cf0732817b2
sai-arkar <iamsaiarkar@gmail.com>
2022-02-11 19:18:43 +0630

    add pagination as a pro

--- apiControllers/appUserPost.js
+++ apiControllers/appUserPost.js
@@ -1,25 +1,47 @@
 const Posts = require("../models/items");
 
-const allPosts = []; /* To Store All Post for Pagination */
+let approvedPosts = []; /* To Store All Post for Pagination */
 
 exports.getPosts =async (req, res, next)=>{
-     const currentPage = req.query.page || 1;
-     const perPage = req.query.perPage || 3;
+     const currentPage = req.query.page;
+     const perPage = req.query.perPage;
 
      try{
-          const totalCount = await Posts.find().countDocuments();
-          let posts = await Posts.find()
-                         .skip((currentPage -1)* perPage)
-                         .limit(perPage)
-          posts.map(post=>{
-               allPosts.push(post);
-          })
-          res.status(200).json({
-               result: allPosts ,
-               currentPage: currentPage,
-               perPage: perPage,
-               totalCount: totalCount
-          });
+          if(currentPage && perPage){
+               let posts = await Posts.find()
+                              .skip((currentPage -1)* perPage)
+                              .limit(perPage);
+               
+               posts.map(post=>{
+                    if(post.state == true){
+                         approvedPosts.push(post);
+                    }
+               })
+
+               res.status(200).json({
+                    result: approvedPosts ,
+                    currentPage: currentPage,
+                    perPage: perPage,
+                    totalCount: approvedPosts.length
+               });
+               approvedPosts = [];
+
+          }else{
+               let posts = await Posts.find()
+          
+               posts.map(post=>{
+                    if(post.state == true){
+                         approvedPosts.push(post);
+                    }
+               })
+
+               res.status(200).json({
+                    result: approvedPosts,
+                    totalCount: approvedPosts.length
+               })
+               approvedPosts = [];
+          }
+          
 
      }catch(err){
           console.log(err);
