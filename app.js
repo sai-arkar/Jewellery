c23f613ce08c9fdf7ca9e4cfb2776796bd3f83fd
sai-arkar <iamsaiarkar@gmail.com>
2022-02-23 16:02:47 +0630

    cors

--- app.js
+++ app.js
@@ -85,6 +85,14 @@ app.use((req, res, next) => {
        });
    });
 
+app.use((req, res, next)=>{
+     res.setHeader('Access-Control-Allow-Origin', '*');
+     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+     res.setHeader('Access-Control-Allow-Credentials', true);
+     next();
+ })
+
 app.use(authRoutes);
 app.use('/admin', adminRoutes);
 app.use('/api', appUserRoutes);
